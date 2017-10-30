bcebdfd39b60356e8e183e228acc099260fdac14
depary <deparyh@gmail.com>
2017-10-30 06:35:16 -0300

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  long: number;
+  lati: number;
+  location = {};
   constructor() { }
 
   ngOnInit() {
-  }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lati = position.coords.latitude;
+        this.long = position.coords.longitude;
+        console.log(position.coords);
+        console.log(this.lati);
+        console.log(this.long);
 
+      });
+  }
+}
+
+getLati() {
+  return this.lati;
+}
+
+getLong() {
+  return this.long;
+
+
+}
 }
